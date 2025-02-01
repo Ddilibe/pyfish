714e905ebea5c2bcbc845048a11986ffa9394195
Ddilibe <franklinfidelugwuowo@gmail.com>
2025-02-02 00:06:40 +0100

    [Feat]: Added a menu feature

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@
 
 mod piece;
 mod engine;
+mod menu;
 
 #[warn(unused_imports, dead_code)]
-use piece::piece::Piece;
-use piece::piece::PieceUnicode;
-use engine::boardgeneration::BoardGeneration;
+// use piece::piece::Piece;
+// use piece::piece::PieceUnicode;
+// use engine::boardgeneration::BoardGeneration;
+use menu::menu::Menu;
 
 
 fn main() {
     // let new_piece = Piece::new(
     //     1, 1, 1, "running", "running",1, PieceUnicode::get_black_bishop()
     // );
-    let mut board = BoardGeneration::new();
-    board.normal_chessboard();
-    board.game_cycle();
+    // let mut board = BoardGeneration::new();
+    // board.normal_chessboard();
+    // board.game_cycle();
     // println!("{}", new_piece);
+    let m = Menu();
+    m.start();
 }
